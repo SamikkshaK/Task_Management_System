627608afd34333cb0440ff5c23ee499a327a7b1a
SamikkshaK <ksamikksha@gmail.com>
2025-07-11 16:27:39 +0530

    not found chsnges

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,2 +1,2 @@
-const NotFound = () => <div className="container mt-4"><h2>404 - Page Not Found</h2></div>;
+const NotFound = () => <div className="container mt-4"><h2>404 - Page Not Found please go to the correct page </h2></div>;
 export default NotFound;
